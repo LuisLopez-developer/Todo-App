fe447da1fefc1cccc05f2825826afd141d4f3838
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-08-31 10:15:21 -0500

    fix: resolved unrecognized processor options error '[dagger.fastInit, kapt.kotlin.generated]'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
 }
 
 android {
