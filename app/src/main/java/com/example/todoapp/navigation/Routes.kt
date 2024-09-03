02db12876b5da97d1d45e5b326a5e8252a405374
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-09-02 23:14:10 -0500

    Add TaskCategoryScreen

--- app/src/main/java/com/example/todoapp/navigation/Routes.kt
+++ app/src/main/java/com/example/todoapp/navigation/Routes.kt
@@ -2,9 +2,6 @@ package com.example.todoapp.navigation
 
 sealed class Routes(val route: String) {
     data object Calendar : Routes("Calendar")
-    data object Pantalla1 : Routes("Pantalla1")
+    data object TaskCategory : Routes("TaskCategory")
     data object Pantalla2 : Routes("Pantalla2")
-    data object Pantalla3 : Routes("Pantalla3/{id}") {
-        fun createRoute(id: Int) = "pantalla3/$id"
-    }
 }
--- app/src/main/java/com/example/todoapp/taskcategory/ui/TaskCategoryScreen.kt
+++ app/src/main/java/com/example/todoapp/taskcategory/ui/TaskCategoryScreen.kt
@@ -1,8 +1,15 @@
 package com.example.todoapp.taskcategory.ui
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.navigation.NavHostController
 
 @Composable
-fun TaskCategoryScreen(){
+fun TaskCategoryScreen(
+    taskCategoryModel: TaskCategoryViewModel,
+    navigationController: NavHostController,
+) {
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+
 
 }
