de535e90490e4d70b17969fb30355e72ebe0f2ba
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-09-02 22:35:55 -0500

    update task

--- app/src/main/java/com/example/todoapp/addtasks/ui/TasksUiState.kt
+++ app/src/main/java/com/example/todoapp/addtasks/ui/TasksUiState.kt
@@ -6,5 +6,4 @@ sealed interface TasksUiState {
     object Loading: TasksUiState
     data class Error(val throwable: Throwable): TasksUiState
     data class Success(val tasks:List<TaskModel>): TasksUiState
-
 }
