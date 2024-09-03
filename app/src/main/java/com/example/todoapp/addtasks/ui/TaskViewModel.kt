729fa599728ae6a5b36b1de047fb945cb65f342e
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-09-02 21:59:31 -0500

    update dependencies

--- app/src/main/java/com/example/todoapp/addtasks/ui/TaskViewModel.kt
+++ app/src/main/java/com/example/todoapp/addtasks/ui/TaskViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todoapp.addtasks.domain.AddTaskUseCase
 import com.example.todoapp.addtasks.domain.DeleteTaskUseCase
 import com.example.todoapp.addtasks.domain.GetTaskUseCase
