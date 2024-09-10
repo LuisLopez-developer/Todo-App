b911f9fb66a87ce3ac5d01404271423bbb52057b
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-09-09 22:40:13 -0500

    Change: from string type to date type data

--- app/src/main/java/com/example/todoapp/addtasks/data/TaskEntity.kt
+++ app/src/main/java/com/example/todoapp/addtasks/data/TaskEntity.kt
@@ -2,6 +2,8 @@ package com.example.todoapp.addtasks.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalTime
 
 @Entity
 data class TaskEntity(
@@ -9,8 +11,8 @@ data class TaskEntity(
     val id: Int,
     val task: String,
     var selected:Boolean = false,
-    val startDate: String? = null,  // Fecha de inicio opcional
-    val endDate: String? = null,    // Fecha de fin opcional
-    val time: String? = null,       // Hora opcional
+    val startDate: LocalDate? = null,  // Fecha de inicio opcional
+    val endDate: LocalDate? = null,    // Fecha de fin opcional
+    val time: LocalTime? = null,       // Hora opcional
     val details: String? = null     // Detalles opcionales
 )
--- app/src/main/java/com/example/todoapp/addtasks/ui/model/TaskModel.kt
+++ app/src/main/java/com/example/todoapp/addtasks/ui/model/TaskModel.kt
@@ -1,11 +1,14 @@
 package com.example.todoapp.addtasks.ui.model
 
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalTime
+
 data class TaskModel(
     val id: Int = System.currentTimeMillis().hashCode(), //Obtener la fecha actual incluido los milisegundos
     val task: String,
     var selected: Boolean = false,
-    val startDate: String? = null,
-    val endDate: String? = null,
-    val time: String? = null,
+    val startDate: LocalDate? = null,
+    val endDate: LocalDate? = null,
+    val time: LocalTime? = null,
     val details: String? = null
 )
