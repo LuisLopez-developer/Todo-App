2780f251ce8d8b6c6c14cabcf087e0aa688392b6
JHANPIERRE <jmunozmanayay@gmail.com>
2024-09-11 11:25:17 -0500

    Rename functions to start with lowercase. Closes #13

--- app/src/main/java/com/example/todoapp/holidays/data/HoliDaysRepository.kt
+++ app/src/main/java/com/example/todoapp/holidays/data/HoliDaysRepository.kt
@@ -1,18 +1,18 @@
 package com.example.todoapp.holidays.data
 
 import com.example.todoapp.holidays.data.network.HoliDaysService
-import com.example.todoapp.holidays.data.network.response.HoliDaysResponse
+import com.example.todoapp.holidays.data.network.response.holiDaysResponse
 
 class HoliDaysRepository {
     private val api = HoliDaysService()
 
     // Obtener todas las festividades
-    suspend fun Holidays(): List<HoliDaysResponse>? {
-        return api.Holidays()
+    suspend fun holidays(): List<holiDaysResponse>? {
+        return api.holidays()
     }
 
     // Obtener festividad por fecha específica
-    suspend fun HolidayByDate(fecha: String): HoliDaysResponse? {
-        return api.HolidayByDate(fecha)
+    suspend fun holidayByDate(fecha: String): holiDaysResponse? {
+        return api.holidayByDate(fecha)
     }
 }
--- app/src/main/java/com/example/todoapp/holidays/data/network/response/HoliDaysResponse.kt
+++ app/src/main/java/com/example/todoapp/holidays/data/network/response/HoliDaysResponse.kt
@@ -2,7 +2,7 @@ package com.example.todoapp.holidays.data.network.response
 
 import com.google.gson.annotations.SerializedName
 
-data class HoliDaysResponse(
+data class holiDaysResponse(
     @SerializedName("fecha") val fecha: String, // Representa la fecha en formato String
     @SerializedName("nombre") val nombre: String // Representa el nombre del día festivo
 )
