bb4413f90b0052cc946933ff88a6373579891920
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-09-12 15:28:37 -0500

    Fix: add Inject constructor

--- app/src/main/java/com/example/todoapp/holidays/domain/GetHolidayByDateUseCase.kt
+++ app/src/main/java/com/example/todoapp/holidays/domain/GetHolidayByDateUseCase.kt
@@ -2,8 +2,9 @@ package com.example.todoapp.holidays.domain
 
 import com.example.todoapp.holidays.data.HoliDaysRepository
 import com.example.todoapp.holidays.ui.model.HolidaysModel
+import javax.inject.Inject
 
-class GetHolidayByDateUseCase {
+class GetHolidayByDateUseCase @Inject constructor() {
     private val repository = HoliDaysRepository()
 
     suspend operator fun invoke(fecha: String): HolidaysModel? = repository.holidayByDate(fecha)
--- app/src/main/java/com/example/todoapp/holidays/domain/GetHolidaysUseCase.kt
+++ app/src/main/java/com/example/todoapp/holidays/domain/GetHolidaysUseCase.kt
@@ -2,8 +2,9 @@ package com.example.todoapp.holidays.domain
 
 import com.example.todoapp.holidays.data.HoliDaysRepository
 import com.example.todoapp.holidays.ui.model.HolidaysModel
+import javax.inject.Inject
 
-class GetHolidaysUseCase {
+class GetHolidaysUseCase @Inject constructor() {
     private val repository = HoliDaysRepository()
 
     suspend operator fun invoke(): List<HolidaysModel>? = repository.holidays()
