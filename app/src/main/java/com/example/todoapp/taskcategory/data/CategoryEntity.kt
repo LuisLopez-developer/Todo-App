9719f754d22d0f55a9b30e6c3dfcfc7a2e60cc14
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-09-28 19:49:27 -0500

    Add: Link the user to the task category

--- app/src/main/java/com/example/todoapp/taskcategory/data/CategoryEntity.kt
+++ app/src/main/java/com/example/todoapp/taskcategory/data/CategoryEntity.kt
@@ -1,15 +1,26 @@
 package com.example.todoapp.taskcategory.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.example.todoapp.settings.auth.data.UserEntity
 import java.util.UUID
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["uid"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class CategoryEntity(
     @PrimaryKey
     val id: String = UUID.randomUUID().toString(),
     val category: String = "",
-    val userId: String? = null
+    val userId: String? = null,
 ) {
     // No-argument constructor for Firestore
     constructor() : this("", "", "")
