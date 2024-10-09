b2ce3ba667b1dc4f9648187efe8f738449eb893b
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-09 08:52:17 -0500

    Add: CardComponent

---
+++ app/src/main/java/com/example/todoapp/ui/components/CardComponent.kt
@@ -0,0 +1,65 @@
+package com.example.todoapp.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardComponent(
+    icon: @Composable () -> Unit,
+    title: String,
+    toggle: @Composable (() -> Unit?)? = null,
+    description: String,
+    onClickText: @Composable (() -> Unit?)? = null,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(15.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(colorScheme.surfaceContainer)
+                .padding(15.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        icon()
+                        Spacer(modifier = Modifier.padding(6.dp))
+                        Text(
+                            text = title,
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+                    }
+                }
+                toggle?.invoke()
+            }
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(10.dp)
+            )
+            Box(Modifier.padding(horizontal =  10.dp)) { onClickText?.invoke() }
+        }
+    }
+}
