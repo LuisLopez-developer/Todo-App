b0e5d792185001a3e6ac17e2cb16a3473da0279a
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-12 21:18:45 -0500

    Fix: RequestNotificationPermissionUseCase

--- app/src/main/java/com/example/todoapp/alarm/domain/RequestNotificationPermissionUseCase.kt
+++ app/src/main/java/com/example/todoapp/alarm/domain/RequestNotificationPermissionUseCase.kt
@@ -1,11 +1,10 @@
 package com.example.todoapp.alarm.domain
 
-import android.content.Context
 import com.example.todoapp.services.permission.PermissionService
 import javax.inject.Inject
 
 class RequestNotificationPermissionUseCase @Inject constructor(private val permissionService: PermissionService) {
-    operator fun invoke(context: Context){
-        permissionService.requestNotificationPermission(context)
+    operator fun invoke() {
+        permissionService.requestNotificationPermission()
     }
 }
